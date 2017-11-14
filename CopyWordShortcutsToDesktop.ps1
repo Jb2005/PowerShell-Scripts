d315f4d20360a941c90e25136b818761242f8a43
Trippy <simplychillin@tutanota.com>
2017-11-13 19:46:04 -0500

    Added desktop shortcut script

---
+++ CopyWordShortcutsToDesktop.ps1
@@ -0,0 +1,7 @@
+﻿###
+#Copy Microsft Word Shortcuts to the desktop.
+###
+
+Copy-Item 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Microsoft Office\Microsoft Excel 2010.lnk' C:\Users\Public\Desktop
+Copy-Item 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Microsoft Office\Microsoft Word 2010.lnk' C:\Users\Public\Desktop
+Copy-Item 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Microsoft Office\Microsoft Publisher 2010.lnk' C:\Users\Public\Desktop
