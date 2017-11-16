d7021422f27530af1d544211192e8a986353ec90
Justin <Jb2005@users.noreply.github.com>
2017-11-16 14:01:38 -0500

    Added comment

--- Function_PasswordLastSet.ps1
+++ Function_PasswordLastSet.ps1
@@ -9,10 +9,15 @@ Import-Module ActiveDirectory
 
 ## Get needed information from user
 $Days = Read-Host "Enter the amount of days you want to check since password has been changed"
+#
+#Will be adding abilty to select OU
 #$OuName = Read-Host "Enter the Organizantional Unit you want to search in."
+#
 $Savefile = Read-Host "File name?"
 
 ## Script
 Get-ADUser -Filter * -Properties Office,DisplayName,PasswordLastSet | Where {$_.PassWordLastSet -le (Get-Date).AddDays(-$Days) } | select Office,DisplayName,PasswordLastSet | Out-File $env:USERPROFILE\Desktop\$Savefile.csv
 
 }
+
+ll
