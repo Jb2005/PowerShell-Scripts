3c9a06e19f75ecd34be19ac7cb1306db825d93f2
Justin <Jb2005@users.noreply.github.com>
2017-11-17 15:35:25 -0500

    Computers last Logon
    Working on making it easier to see how many computers have not logged in in x number of days.

---
+++ CheckComputersLastLogon.ps1
@@ -0,0 +1,12 @@
+﻿###
+# List computers not logged onto in x numbers of days.
+# 
+###
+
+$FileName = Read-Host "What do you want to name the file?"
+$DayInactive = Read-Host "What is your last logon time in days past"
+$Time = (Get-Date).AddDays(-($DayInactive))
+
+
+Get-ADComputer -Filter {LastLogonDate -lt $Time} -Properties LastLogonDate | select Name,LastLogonDate | Out-file C:\Users\itmaint\Desktop\$Filename.csv
+
