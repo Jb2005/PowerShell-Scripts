669755f120e5c2e52eff44ce8d7cea52a81fca3e
Justin <Jb2005@users.noreply.github.com>
2017-12-08 13:43:03 -0500

    Reset Password
    A simple way for me to reset a user's password.

---
+++ Function_ResetPassword.ps1
@@ -0,0 +1,13 @@
+﻿Function Reset-UserPassord {
+####
+#For reseting passwords
+###
+
+$samAccountName = Read-Host "What is the SamAccountName?"
+
+$newpassword = Read-Host "What is the New Password?"
+
+
+$newpwd = ConvertTo-SecureString -String $newpassword -AsPlainText -Force
+Set-ADAccountPassword $samAccountName -NewPassword $newpwd -Reset
+}
