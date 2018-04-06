ba8f3c9ffcddf37d6c7e513bd97edf9bc371564f
Justin <Jb2005@users.noreply.github.com>
2018-04-06 15:30:27 -0400

    Update
    Added help section

--- Function_FindGroup.ps1
+++ Function_FindGroup.ps1
@@ -1,9 +1,18 @@
+<#
+.SYNOPSIS
+Queries ActiveDirectory for a group name based on the part you know.
+.DESCRIPTION
+Finds the Groups Name and Description. This excepts wildcards.
+.PARAMETER SamAccountName
+The name of the ActiveDirectory account you want to query.
+.EXAMPLE
+Find-Group VPN*
+#>
 
-function Find-Group ($GroupNamepart){
-
-###
-# Help to find group name
-###
+function Find-Group{
+    Param(
+        [string]$GroupNamePart
+        )
 
 Get-ADGroup -Filter "name -like'$GroupNamepart'" -Properties Description  | select Name,Description
 }
