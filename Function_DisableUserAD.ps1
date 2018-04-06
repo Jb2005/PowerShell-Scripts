8b3cd7aa936dc94f21f7bc4f595b4626a756c81d
Justin <Jb2005@users.noreply.github.com>
2018-04-06 14:52:42 -0400

    Clean up
    ALso added a better way to pick the Organizational Unit.

--- Function_DisableUserAD.ps1
+++ Function_DisableUserAD.ps1
@@ -1,40 +1,41 @@
 Function Disable-User {
 ######
-# This will disable the user and move them to the disabled user's OU
+# This will disable the user and move them to the Organizational Unit of your choice.
 ######
 
-#### makes sure the Activedirectory module is loaded.
-Import-Module Activedirectory
+#makes sure the Activedirectory module is loaded.
+    Import-Module Activedirectory
 
-#### Imput the employees user name
-$User = Read-Host "Enter the SamAccountName of the person to be disabled in the system."
+#Imput the employees user name
+    $User = Read-Host "Enter the SamAccountName of the person to be disabled in the system."
 
-#### Get information about the user
-$Username = Get-ADUser -Identity $User -Properties memberOf
+#Imput where you want to the account to go.
+    $Ou = Read-Host "Enter the DistinguishedName of the Organizational Unit you want the account to go into."
 
-#### Get a list of groups that the user is a memebr of
-$Groups = $Username.memberOf | ForEach-Object {
-    Get-ADGroup $_
-}
+#Get information about the user
+    $Username =  Get-ADUser -Identity $User -Properties memberOf
 
-#### Go through the groups and remove the user
-$Groups | ForEach-Object { Remove-ADGroupMember -Identity $_ -Members $Username -Confirm:$false }
+#Get a list of groups that the user is a memebr of
+    $Groups = $Username.memberOf | ForEach-Object {
+        Get-ADGroup $_
+        }
+
+#Go through the groups and remove the user
+    $Groups | ForEach-Object { Remove-ADGroupMember -Identity $_ -Members $Usernamer -Confirm:$false }
 
 
-#### Move the user to the Disabled User OU
-## --->>> UPDATE belowe for your envoronment
-Get-ADUser $User | Move-ADObject -TargetPath 'OU=whereYou,DC=wantThem,DC=toGo'
+#Move the user to the Disabled User OU
+    Get-ADUser $User | Move-ADObject -TargetPath "$Ou"
 
-#### Disable the user's account
-Disable-ADAccount -Identity $User
+#Disable the user's account
+    Disable-ADAccount -Identity $User
 
-#### Pull the user's information to make sure everything is done correctly.
-Get-ADUser $User -Properties DistinguishedName,Enabled,Officephone,Office,Displayname,Emailaddress,Description,Title | Format-List DistinguishedName,Enabled,OfficePhone,Office,Displayname,emailaddress,Description,Title
+#Pull the user's information to make sure everything is done correctly.
+    Get-ADUser $User -Properties DistinguishedName,Enabled | 
+        Format-List DistinguishedName,Enabled
 
-#### Reminder check list
+#Reminder check list
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-echo "Did you Disabel the Phone?"
-echo "!!!!"
 echo " "
 echo "Did you shut off the email?"
 echo "!!!!"
