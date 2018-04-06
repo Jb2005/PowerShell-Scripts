735796fddb8f0cd7eed5cbfdd8287e0e8787991f
Justin <Jb2005@users.noreply.github.com>
2018-04-05 20:15:40 -0400

    Updating help section
    Taking class this week and they showed us how to add to the help part. My first try at it

--- Function_getUserInfo .ps1
+++ Function_getUserInfo .ps1
@@ -1,6 +1,17 @@
-####
-#This takes one argument from the user
-#####
+<#
+.SYNOPSIS
+Queries ActiveDirectory for informaiont about an AD account
+.DESCRIPTION
+Use this to pull Active Directory account information.
+.PARAMETER SamAccountName
+The name of the ActiveDirectory account you want to query.
+.EXAMPLE
+Get-UserInfo BSnoopy
+
+This takes one argument from the user
+
+#>
+
 Function Get-UserInfo ($User) {
 
 Get-ADUser $User -Properties DistinguishedName,Enabled,Officephone,Office,Displayname,Emailaddress,Description,Title | Format-List DistinguishedName,Enabled,OfficePhone,Office,Displayname,emailaddress,Description,Title
