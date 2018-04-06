4bac13148c60c47815245878759c0efc9f04d917
Justin <Jb2005@users.noreply.github.com>
2018-04-06 15:06:26 -0400

    Clean up

--- Function_FindUserName.ps1
+++ Function_FindUserName.ps1
@@ -1,9 +1,22 @@
-###
-# Help to find a user's SammAccountName
-###
-function Find-UserName ($Namepart){
+<#
+.SYNOPSIS
+Queries ActiveDirectory for a user's name based on the part you know.
+.DESCRIPTION
+Finds the users name and SamAccountName. This excepts wildcards.
+.PARAMETER SamAccountName
+The name of the ActiveDirectory account you want to query.
+.EXAMPLE
+Find-UserName SNo*
+
+This takes one argument from the user
+
+#>
+
+function Find-UserName {
+    Param(
+        [string]$Namepart
+        )
 
 # Get the information.
-
-Get-ADUser -Filter "name -like'$Namepart'"  | select Name,SamAccountName
+    Get-ADUser -Filter "name -like'$Namepart'"  | select Name,SamAccountName
 }
