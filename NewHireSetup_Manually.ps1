112dbd5bdea13c74a38923c2b6ae6463e57a35db
Justin <Jb2005@users.noreply.github.com>
2018-04-06 09:05:37 -0400

    Edited for sharing

---
+++ NewHireSetup_Manually.ps1
@@ -0,0 +1,47 @@
+####
+# Run this as domain admin
+# This adds user's to groups and fills in the needed spots in the Active Directory profile information.
+# You can get the infromation from the New Hire Cheet sheet.
+####
+
+# Makes sure the ActiveDirectory cmdlets are loaded
+Import-Module ActiveDirectory 
+
+# Lets the user know information about the script
+echo " "
+echo "This is for filling out the Active Directory part of the new hire process."
+echo " "
+
+echo " "
+echo "When adding email addresses do not includ the @domain.org part."
+echo "For example for if you wanted to add the All_Staff@doamin.org you would just enter All_Staff."
+
+
+# Gets the user to work with and information needed to fill out the Active Directory profile.
+$User = Read-Host "Enter users log on name"
+$Description = Read-Host "Enter the users program name"
+$Office = Read-Host "Enter site name"
+$OfficePhone = Read-Host "Enter phone extesion"
+
+#Files in the new ADUsers profile
+    Set-ADUser -Identity $User -Office $Office -OfficePhone $OfficePhone -Description $Description -Title $Description -Server Admin-DC1
+
+#Asks if you need to add any groups until you say no.
+    while ($Groups = (Read-Host "Add this user to any security groups? [yes or no]") -ne 'no') {
+        $GroupName = Read-Host "Enter the group name"
+        Add-ADGroupMember $GroupName -Members $User 
+    }
+
+
+##Below adds the new user to the requested email groups.
+    while($EGroups = (Read-Host "Add this user to any email groups? [yes or no]") -ne 'no') {
+        $EGroupName = Read-Host "Enter email group name."
+        Add-ADGroupMember -Identity $EGroupName -Members $User
+    }
+
+ 
+
+#prints out needed information for email to a txt file on the desktop.
+Get-ADUser -Identity $User  -Properties DistinguishedName,Officephone,Displayname,Emailaddress,Description,Title | Format-List DistinguishedName,OfficePhone,Displayname,emailaddress,Description,Title | Out-File C:\Users\jpolnack\Desktop\$User.txt
+
+Read-Host 'Press any key to continue'
