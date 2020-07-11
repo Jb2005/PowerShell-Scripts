63a6b4715bd188bde9d730a93b89a2e120d315ca
Justine <Jb2005@users.noreply.github.com>
2020-07-11 10:45:24 -0400

    Clean up

--- PC-LastLogon.ps1
+++ PC-LastLogon.ps1
@@ -10,6 +10,6 @@ $DayInactive = Read-Host "What is your last logon time in days past"
 $Time = (Get-Date).AddDays(-($DayInactive))
 
 
-Get-ADComputer -Filter {LastLogonDate -lt $Time} -Properties LastLogonDate | select Name,LastLogonDate | Out-file $env:USERPROFILE\Desktop\$Filename.csv
+Get-ADComputer -Filter {LastLogonDate -lt $Time} -Properties LastLogonDate | Select-Object Name,LastLogonDate | Out-file $env:USERPROFILE\Desktop\$Filename.csv
 }
 
--- Password-LastSet.ps1
+++ Password-LastSet.ps1
@@ -16,7 +16,7 @@ $Days = Read-Host "Enter the amount of days you want to check since password has
 $Savefile = Read-Host "File name?"
 
 ## Script
-Get-ADUser -Filter {(Enabled -eq "True")} | ? { ($_.distinguishedname -notlike '*DSADMINS*') -or ($_.DistinguishedName -notlike "Builtin*") } -Properties Office,DisplayName,PasswordLastSet | Where {$_.PassWordLastSet -le (Get-Date).AddDays(-$Days) } | select Office,DisplayName,PasswordLastSet | Out-File $env:USERPROFILE\Desktop\$Savefile.csv
+Get-ADUser -Filter {(Enabled -eq "True")} | ? { ($_.distinguishedname -notlike '*DSADMINS*') -or ($_.DistinguishedName -notlike "Builtin*") } -Properties Office,DisplayName,PasswordLastSet | Where {$_.PassWordLastSet -le (Get-Date).AddDays(-$Days) } | Select-Object Office,DisplayName,PasswordLastSet | Out-File $env:USERPROFILE\Desktop\$Savefile.csv
 
 }
 
