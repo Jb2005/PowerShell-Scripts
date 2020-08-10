0140261290955244f8f8faf43bccbc0ae7a8f784
Justine <Jb2005@users.noreply.github.com>
2020-08-10 19:21:02 -0400

    learning

--- Hello-Parameters/Hello-Parameters.ps1
+++ Hello-Parameters/Hello-Parameters.ps1
@@ -1,7 +1,13 @@
 
 
-function Hello-Parameters {
-    param(
+<#
+    When I have them up here before the function is works.
+            Because
+    Here you are saying let us look at these param first.
+    If you fill them we will us the information.
+    If any are mandatory you must us them
+#>
+   param(
     [Parameter(Mandatory)]
 ##  Stating we have a mandatory parameter
     [string]$Cats,
@@ -9,8 +15,26 @@ function Hello-Parameters {
     [Parameter()]
 ##  Stating the parameter is optional
     [string]$Parameter2    ## Don't need the ',' comma after the last parameter
+)
 
- )
+function Hello-Parameters 
+{
+ 
+<#
+    When I put them here at the start of the function while it does force you to suppley a
+    value for $Cats, you cannot add before.
+            Because
+    Here it runs the script and then asked for param.   
+#>
+    # param(
+    #     [Parameter(Mandatory)]
+    # ##  Stating we have a mandatory parameter
+    #     [string]$Cats,
+    # ##  [type]$VeriableName, 
+    #     [Parameter()]
+    # ##  Stating the parameter is optional
+    #     [string]$Parameter2    ## Don't need the ',' comma after the last parameter
+    # )
  
  Write-Host "Parameter 1 value is $Cats"
 
