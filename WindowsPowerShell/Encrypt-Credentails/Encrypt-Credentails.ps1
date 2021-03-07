de5332610a48636271ddcbb690b30534df4d11d7
Justine <Jb2005@users.noreply.github.com>
2021-03-07 10:56:50 -0500

    Function created
    A function I created for a work project. Well for work I only needed to encrypt creds for the script. I think it is a good idea to make it a function as I'm sure I will need it again.

---
+++ WindowsPowerShell/Encrypt-Credentails/Encrypt-Credentails.ps1
@@ -0,0 +1,45 @@
+﻿
+
+function Encrypt-Credentails {
+    [CmdletBinding()]
+    param (        
+        [Parameter(Mandatory=$true,
+        ValueFromPipelineByPropertyName=$true,
+        HelpMessage="Enter the path to save the generated CSV file.")]
+        $FilePathForcsv
+    )
+    
+    # for encription creds
+$table = New-Object System.Data.DataTable "Credentials"
+$col1 = New-Object System.Data.DataColumn Username
+$col2 = New-Object System.Data.DataColumn Password
+$table.Columns.Add($col1)
+$table.Columns.Add($col2)
+
+
+
+$credentials = Get-credential -Message "enter creds"
+$credUser = $credentials.UserName
+$credPass = $Credentials.Password | ConvertFrom-SecureString
+
+$credpath = "$FilePathForcsv.csv"
+
+$row = $table.NewRow()
+$row.Username = $credUser
+$row.Password = $credPass
+$table.Rows.Add($row)
+
+$table | Export-csv $credpath -NoTypeInformation
+}
+
+Encrypt-Credentails
+
+
+
+
+
+# Add below to the file that needs the creds
+<#
+$Credcsv = import-csv -Path "<Path\to\file.csv>"
+$Creds = New-Object System.Management.Automation.PSCredential ($Credcsv.Username,(ConvertTo-SecureString $Credcsv.Password))
+#>
